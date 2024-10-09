f2270a27c8590a356d1cdb773d8399bf0ea2e8f9
Omar Villamizar <omarjesusvi@ufps.edu.co>
2024-10-09 18:45:27 -0500

    add: professor entity created and port defined to 8091

--- src/main/java/com/example/chaea/entities/Profesor.java
+++ src/main/java/com/example/chaea/entities/Profesor.java
@@ -1,12 +1,28 @@
 package com.example.chaea.entities;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 
 @Entity
+@Table(name = "profesor")
 public class Profesor {
 
 	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private int id;
 	
+	@Column(length = 100,nullable = false)
+	private String usuario_email;
+	
+	@Column(length = 50,nullable = false)
+	private String carrera;
+	
+	private int id_rol;
+	
+	@Column(length = 100,nullable = false)
+	private String estado;
 }
