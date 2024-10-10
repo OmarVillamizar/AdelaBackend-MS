678a3724b7668c38d043dc597213ebb1993359ad
Omar Villamizar <omarjesusvi@ufps.edu.co>
2024-10-10 11:03:09 -0500

    feat : unnecessary imports deleted

--- src/main/java/com/example/chaea/entities/Estudiante.java
+++ src/main/java/com/example/chaea/entities/Estudiante.java
@@ -2,7 +2,6 @@ package com.example.chaea.entities;
 
 import java.sql.Date;
 
-import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
