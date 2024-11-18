c44bb6415fb1bc37155c7d9d367728ea9b96de15
Omar Villamizar <omarjesusvi@ufps.edu.co>
2024-11-17 19:40:58 -0500

    feat : category entity completed

--- src/main/java/com/example/chaea/entities/Categoria.java
+++ src/main/java/com/example/chaea/entities/Categoria.java
@@ -1,5 +1,7 @@
 package com.example.chaea.entities;
 
+import com.fasterxml.jackson.annotation.JsonBackReference;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
@@ -7,14 +9,17 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import lombok.ToString;
 
 @Entity
 @AllArgsConstructor
 @NoArgsConstructor
 @Data
+@Table(name = "estilo")
 public class Categoria {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -22,6 +27,8 @@ public class Categoria {
     
     @ManyToOne
     @JoinColumn(name = "cuestionario_id", referencedColumnName = "id", nullable = false)
+    @JsonBackReference
+    @ToString.Exclude
     private Cuestionario cuestionario;
     
     @Column(nullable = false)
