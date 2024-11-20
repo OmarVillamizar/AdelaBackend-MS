d6c49a129b9ed60ce1d1892fcadbe7e6a5e2b309
JorgeMarles <jamarlesf@gmail.com>
2024-11-20 14:57:14 -0500

    fix: add nombre field for grupoDTO, create EstudianteCrearDTO

---
+++ src/main/java/com/example/chaea/dto/EstudianteCrearDTO.java
@@ -0,0 +1,11 @@
+package com.example.chaea.dto;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+public class EstudianteCrearDTO {
+    private String nombre;
+    private String email;
+}
