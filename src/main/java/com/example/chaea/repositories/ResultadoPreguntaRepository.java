7380788797a0302777053a24d0a1f6cfb3dc7f96
Omar Villamizar <omarjesusvi@ufps.edu.co>
2024-11-22 10:01:07 -0500

    feat: changes on delete questionnarie

--- src/main/java/com/example/chaea/repositories/ResultadoPreguntaRepository.java
+++ src/main/java/com/example/chaea/repositories/ResultadoPreguntaRepository.java
@@ -1,9 +1,14 @@
 package com.example.chaea.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.transaction.annotation.Transactional;
 
+import com.example.chaea.entities.Opcion;
 import com.example.chaea.entities.ResultadoPregunta;
 
 public interface ResultadoPreguntaRepository extends JpaRepository<ResultadoPregunta, Long>{
-    
+	@Transactional @Modifying @Query("DELETE FROM ResultadoPregunta rp WHERE rp.opcion = :opcion") 
+	void deleteByOpcion(Opcion opcion); 
 }
